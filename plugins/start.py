078efb773efe37431ca6daf403715f0887ee7b25
Siam <ssiam4235@gmail.com>
2024-04-12 02:11:28 +0600

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -5,17 +5,17 @@ startText = """
 ᴛʜᴇsᴇ ᴀʀᴇ sᴏᴍᴇ ᴀɪ ᴄᴏᴍᴍᴀɴᴅs
 
  ➻ /draw : ᴄʀᴇᴀᴛᴇ ɪᴍᴀɢᴇs
- ➻ /upscale : ᴜᴘsᴄᴀʟᴇ ʏᴏᴜʀ ɪᴍᴀɢᴇs
+ ➻ /ups : ᴜᴘsᴄᴀʟᴇ ʏᴏᴜʀ ɪᴍᴀɢᴇs
  ➻ /gpt : ᴄʜᴀᴛɢᴘᴛ
  ➻ /bard : ʙᴀʀᴅ ᴀɪ ʙʏ ɢᴏᴏɢʟᴇ
  ➻ /mistral : ᴍɪsᴛʀᴀʟ ᴀɪ
  ➻ /llama : ʟʟᴀᴍᴀ ʙʏ ᴍᴇᴛᴀ ᴀɪ
  ➻ /palm : ᴘᴀʟᴍ ʙʏ ɢᴏᴏɢʟᴇ
- ➻ /reverse : ʀᴇᴠᴇʀsᴇ ɪᴍᴀɢᴇ sᴇᴀʀᴄʜ
+ ➻ /pp : ʀᴇᴠᴇʀsᴇ ɪᴍᴀɢᴇ sᴇᴀʀᴄʜ
  ➻ /gemini : ɢᴇᴍɪɴɪ ʙʏ ɢᴏᴏɢʟᴇ
- ➻ /imagine : ᴄʀᴇᴀᴛᴇ ᴀɪ ɪᴍᴀɢᴇs
+ ➻ /img : ᴄʀᴇᴀᴛᴇ ᴀɪ ɪᴍᴀɢᴇs
 
- ➻ sᴜᴘᴘᴏʀᴛ ᴄʜᴀᴛ - @weebs_support
+ ➻ sᴜᴘᴘᴏʀᴛ ᴄʜᴀᴛ - @AnimeKunChannel
 """
 
 @Client.on_message(filters.command(["start","help","repo","source"]))
@@ -25,7 +25,7 @@ async def start(_: Client, m: t.Message):
         reply_markup=t.InlineKeyboardMarkup(
             [
                 [
-                    t.InlineKeyboardButton(text="ᴊᴏɪɴ ᴍy ᴜᴩᴅᴀᴛᴇ ᴄʜᴀɴɴᴇʟ",url="https://t.me/codeflix_bots")
+                    t.InlineKeyboardButton(text="ᴊᴏɪɴ ᴍy ᴄʜᴀɴɴᴇʟ",url="https://t.me/Anime_Kun_Channel")
                 ]
             ]
         )
