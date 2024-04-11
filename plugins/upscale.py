e97cfc321f970607c94fd5437ea10bb2e835f0db
Siam <ssiam4235@gmail.com>
2024-04-12 02:09:46 +0600

    Update upscale.py

--- plugins/upscale.py
+++ plugins/upscale.py
@@ -2,14 +2,14 @@ from pyrogram import Client, filters, types as t
 from Utils import getFile, UpscaleImages
 import os
 
-@Client.on_message(filters.command(["upscale"]))
+@Client.on_message(filters.command(["ups"]))
 async def upscaleImages(_, message):
     file = await getFile(message)
     if file == 1:
        return await message.reply_text("File size is large")
     if file is None:
        return await message.reply_text("Replay to an image?")
-    msg = await message.reply("wait a min...")
+    msg = await message.reply("Wait...")
     imageBytes = open(file,"rb").read()
     os.remove(file)
     upscaledImage = await UpscaleImages(imageBytes)
